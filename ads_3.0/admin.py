89f22585bb476f1685bf30394ee13dd18be15935
Anirudha B Shetty <45870174+ab007shetty@users.noreply.github.com>
2020-08-20 22:31:25 +0530

    DJ4E

---
+++ ads_3.0/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from ads.models import Ad
+
+# Register your models here.
+
+admin.site.register(Ad)
---
+++ mysite/github_settings.py
@@ -0,0 +1,34 @@
+
+# Copy this file to github_settings.py (don't check it into github)
+
+# Go to https://github.com/settings/developers
+
+# Add a New OAuth2 App 
+
+# Using ngrok is hard because the url changes every time you start ngrok
+
+# If you are running on localhost, here are some settings:
+
+# Application name: ChuckList Local
+# Homepage Url: http://localhost:8000
+# Application Description: Whatever
+# Authorization callback URL: http://127.0.0.1:8000/oauth/complete/github/
+
+
+# Using PythonAnywhere here are some settings:
+
+# Application name: ChuckList PythonAnywhere
+# Homepage Url: https://drchuck.pythonanywhere.com
+# Application Description: Whatever
+# Authorization callback URL: https://drchuck.pythonanywhere.com/oauth/complete/github/
+
+# Also on PythonAnywhere, go into the Web tab and enable "Force HTTPS"
+# so you don't get a redirect URI mismatch.
+
+# Then copy the client_key and secret to this file
+
+SOCIAL_AUTH_GITHUB_KEY = '5210edc88637de858392'
+SOCIAL_AUTH_GITHUB_SECRET = '43d5ce2dfb12a2db364e92989220733ca6fcf8cc'
+
+# For detail: https://readthedocs.org/projects/python-social-auth/downloads/pdf/latest/
+
